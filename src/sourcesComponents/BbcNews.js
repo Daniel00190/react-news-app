7e29ea2648c9e09d1fe0612dcefbaff2a036bf31
Daniel <Jjsnow00190@protonmail.com>
2022-02-03 21:43:23 -0500

    no material ui

--- src/sourcesComponents/BbcNews.js
+++ src/sourcesComponents/BbcNews.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ComPage from "../subRoutes/comPage";
 
 import useFetch from "../subComponents/myFetchHook";
--- src/subRoutes/Health.js
+++ src/subRoutes/Health.js
@@ -1,15 +1,6 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-
-import Typography from "@material-ui/core/Typography";
 
 import useFetch from "../subComponents/myFetchHook";
-import LinearDeterminate from "../subComponents/LinearDeterminate";
 import ComPage from "./comPage";
 
 export default function Health() {
