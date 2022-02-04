581c4df36282ef815e39c64e1346a944a1ca4dab
Daniel <Jjsnow00190@protonmail.com>
2022-02-04 11:09:31 -0500

    build folder

---
+++ build/precache-manifest.318ca1fbf7117f900845361736d0f94e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a698b3a65b3e9bea2e2acd1cddeab3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43c06d1ad28067e4f01d",
+    "url": "/static/css/main.c93419cf.chunk.css"
+  },
+  {
+    "revision": "e9f1fbfbefd0dc51350a",
+    "url": "/static/js/2.46a8986b.chunk.js"
+  },
+  {
+    "revision": "1bf912bc80172ed5a8edbabfec6669f0",
+    "url": "/static/js/2.46a8986b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43c06d1ad28067e4f01d",
+    "url": "/static/js/main.b61397a7.chunk.js"
+  },
+  {
+    "revision": "f757fe882ad56bf13027",
+    "url": "/static/js/runtime-main.386c8635.js"
+  }
+]);
