169eda876fa96140af55bf491c249013ca8b1cd1
Daniel <Jjsnow00190@protonmail.com>
2022-02-21 11:21:05 -0500

    Update LinearDeterminate.js

--- src/subComponents/LinearDeterminate.js
+++ src/subComponents/LinearDeterminate.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import CircularProgress from '@material-ui/core/CircularProgress';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    '& > * + *': {
-      marginLeft: theme.spacing(2),
-    },
-  },
+   root: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "15rem",
+      width: "100%",
+      "& > * + *": {
+         marginLeft: theme.spacing(2),
+      },
+   },
 }));
 
 export default function CircularIndeterminate() {
-  const classes = useStyles();
+   const classes = useStyles();
 
-  return (
-    <div className={classes.root}>
-      <CircularProgress />
-      
-    </div>
-  );
+   return <div className={classes.root}>LOADING....</div>;
 }
